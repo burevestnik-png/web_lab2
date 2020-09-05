89f7f8be5e449958e030d684ad52e105a9721138
Yaroslav <yaroslavsoldat2@gmail.com>
2020-09-05 16:27:05 +0300

    Added handling expections

--- src/main/java/servlets/ControllerServlet.java
+++ src/main/java/servlets/ControllerServlet.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int x = 3 / 0;
         getServletContext()
                 .getRequestDispatcher("/index.jsp")
                 .forward(req, resp);
